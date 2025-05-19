41a57a2bb26b2dfdc67d84267415326956e4d4f5
Villadskw1 <villadskohler@outlook.dk>
2025-05-19 13:19:07 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,37 @@
+function getUsers() {
+  return JSON.parse(localStorage.getItem('users') || '{}');
+}
 
+function saveUser(username, password) {
+  const users = getUsers();
+  if (users[username]) {
+    return false;
+  }
+  users[username] = { password, progress: {} };
+  localStorage.setItem('users', JSON.stringify(users));
+  return true;
+}
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const users = getUsers();
+
+  if (users[username] && users[username].password === password) {
+    localStorage.setItem('currentUser', username);
+    window.location.href = "game.html";
+  } else {
+    document.getElementById("message").innerText = "Login fejlede!";
+  }
+}
+
+function register() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (saveUser(username, password)) {
+    document.getElementById("message").innerText = "Bruger oprettet. Du kan nu logge ind.";
+  } else {
+    document.getElementById("message").innerText = "Brugernavn findes allerede!";
+  }
+}
